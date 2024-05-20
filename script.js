1283f6b9368e1308beba8f3b898374ac8a10bb69
bprasad156 <156602266+bprasad156@users.noreply.github.com>
2024-05-20 16:50:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+    for(let i=0;i<s1.length();i++){
+    let  ans =s1.substring(i+1,s1)
+        for(let j=i+1;j<s1.length();j++){
+              let an=s2.substring(i,j);
+            
+             if(ans<1){
+                ans=a[i];
+             }
+             else if(an<1){
+              an=[i];
+             }
+             else{
+                 return -1;
+             }
+                
+             
+
+        }
+    }
+
 }
 
 // Please do not change the code below
